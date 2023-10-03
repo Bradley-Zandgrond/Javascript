43f1bde2b5a5a83567ab6bf70eb284d2c0f41d6b
Bradley-Zandgrond <9021118@student.zadkine.nl>
2023-10-03 09:57:18 +0200

    calculate minutes

--- Opdracht 6.1/code.js
+++ Opdracht 6.1/code.js
@@ -1,6 +1,6 @@
 function changeColor()
 {
-    console.log("kleur veranderen")
+    console.log("kleur veranderen");
     let kleur = prompt("Choose your font color", "");
     document.getElementById("kop1").style.color = kleur ;
 }
@@ -13,4 +13,24 @@ function changeText()
 function changeText1()
 {
     document.getElementById('kop1').innerHTML = "SOD1C" ;
+}
+
+function multiplyByFive()
+{
+    let inputNumber = prompt("multiply by five?", "");
+    var result = inputNumber * 5;
+    console.log(result);
+    alert(result)
+}
+
+function calculateMinutes()
+{
+    let inputSecond = prompt("Second to Minute");
+    var awnser = inputSecond / 60;
+    var slash = "/"
+    var is = "="
+    var number = "60"
+    var minutes = "Minutes"
+    console.log(inputSecond,slash,number,is,awnser,minutes);
+    alert(awnser,minutes);
 }
